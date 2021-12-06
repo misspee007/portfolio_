fc39b94ba39f49bfdbb9d270e45ac752ae4931c1
Precious Abubakar <misspee007@gmail.com>
2021-12-06 15:37:48 +0100

    Updates

---
+++ precache-manifest.eb72d064ca581d3bf5a2932ca232bc07.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3766d99d774a62145d120c495cc7105f",
+    "url": "/portfolio_/index.html"
+  },
+  {
+    "revision": "b94f72328427952187fa",
+    "url": "/portfolio_/static/css/2.9c389199.chunk.css"
+  },
+  {
+    "revision": "fe2abd69222d7180633d",
+    "url": "/portfolio_/static/css/main.8653b40b.chunk.css"
+  },
+  {
+    "revision": "b94f72328427952187fa",
+    "url": "/portfolio_/static/js/2.365b8202.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "/portfolio_/static/js/2.365b8202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe2abd69222d7180633d",
+    "url": "/portfolio_/static/js/main.588ed029.chunk.js"
+  },
+  {
+    "revision": "1fd79273fcf581e63cfd",
+    "url": "/portfolio_/static/js/runtime-main.e7ec6f7e.js"
+  },
+  {
+    "revision": "bdbdbf2872cf23eed2393b0098dc2064",
+    "url": "/portfolio_/static/media/cart.bdbdbf28.jpg"
+  },
+  {
+    "revision": "cf449c49567d0bfe104fa9286a6dc74c",
+    "url": "/portfolio_/static/media/cart.cf449c49.png"
+  },
+  {
+    "revision": "0346bc32b26781fd0fd7e1a3505cda30",
+    "url": "/portfolio_/static/media/contact.0346bc32.jpg"
+  },
+  {
+    "revision": "185b1943aec5df652d1c12cd8d3637d0",
+    "url": "/portfolio_/static/media/draw.185b1943.jpg"
+  },
+  {
+    "revision": "f98f85316592fec0f870703d1e7b9eaa",
+    "url": "/portfolio_/static/media/game.f98f8531.jpg"
+  },
+  {
+    "revision": "f1a157c908482304648e9a637058cd91",
+    "url": "/portfolio_/static/media/home.f1a157c9.jpg"
+  },
+  {
+    "revision": "6c4d3fd6274716fe1600411f2f758175",
+    "url": "/portfolio_/static/media/homeTeach.6c4d3fd6.jpg"
+  },
+  {
+    "revision": "b83a03c79b457043cc243d0b8bebb475",
+    "url": "/portfolio_/static/media/loss.b83a03c7.jpg"
+  },
+  {
+    "revision": "ebf01a4462bf0bb1832b1391922141b9",
+    "url": "/portfolio_/static/media/myProfile.ebf01a44.png"
+  },
+  {
+    "revision": "fb6b0cc91d6d718dc3882285ad6efd43",
+    "url": "/portfolio_/static/media/win.fb6b0cc9.jpg"
+  },
+  {
+    "revision": "3875672c8aa77faa66c1065797c86fda",
+    "url": "/portfolio_/static/media/zcChess.3875672c.png"
+  },
+  {
+    "revision": "4456e9a7cfd22104a518a7b5e32e8052",
+    "url": "/portfolio_/static/media/zcLogo.4456e9a7.svg"
+  },
+  {
+    "revision": "3f36120530c1c48bb6e19f6006a3d053",
+    "url": "/portfolio_/static/media/zcManageDAO.3f361205.png"
+  },
+  {
+    "revision": "8af96d6b24143ce5c8c211698502aed8",
+    "url": "/portfolio_/static/media/zcVirtualWorkspaces.8af96d6b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.e93a955418308c6a5ae027586babc56f.js"
+  "/portfolio_/precache-manifest.eb72d064ca581d3bf5a2932ca232bc07.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio_/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
